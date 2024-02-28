d2c154ee1795f25b91f4de7b32b4d91bda7087fe
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2024-02-27 23:32:20 -0500

    paragraph styling

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -6,7 +6,7 @@ export const Home = () => {
         <div className="home-page"> 
             <p className="about-me"> Hi! My name is Ella. <br />
                 I'm a full stack software designer based in New York City. <br /><br />
-              I NURTURE brands,<br /> CULTIVATE new connections,<br />and GROW vibrant communities online.
+              I &nbsp;NURTURE&nbsp; brands,&nbsp;CULTIVATE&nbsp; new connections, and &nbsp;GROW&nbsp; vibrant communities online.
             </p>
         </div>
     );
