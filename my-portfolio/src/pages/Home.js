89a21f9e18c8f7927f1e113d2a678c582ce80a9d
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2024-03-02 16:09:17 -0500

    website layout styling

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -7,9 +7,9 @@ export const Home = () => {
         <div className="home-page"> 
             <div>
             <p class="about-me">
-            Hey there! My name is <span class="highlight">Ella</span>. <br />
+            Hey there! My name is <span class="highlight">Ella</span>.
             I'm a full stack software designer based in New York City. <br /><br />
-            I &nbsp;<span class="highlight">nurture</span>&nbsp; brands, &nbsp;<span class="highlight">cultivate</span>&nbsp; new connections, and &nbsp;<span class="highlight">grow</span>&nbsp; vibrant communities online.
+            I <span class="highlight">nurture</span> brands, <span class="highlight">cultivate</span> new connections, and <span class="highlight">grow</span> vibrant communities online.
             </p>
             </div>
             <div className="watering-can">
