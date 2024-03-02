57ac77cf0219cef56620b4ad95ffffffb1f4825c
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2024-03-02 14:51:25 -0500

    font styling

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -6,9 +6,10 @@ export const Home = () => {
     return (
         <div className="home-page"> 
             <div>
-            <p className="about-me"> Hi! My name is Ella. <br />
-                I'm a full stack software designer based in New York City. <br /><br />
-              I nurture brands, cultivate new connections, and grow vibrant communities online.
+            <p class="about-me">
+            Hey there! My name is <span class="highlight">Ella</span>. <br />
+            I'm a full stack software designer based in New York City. <br /><br />
+            I &nbsp;<span class="highlight">nurture</span>&nbsp; brands, &nbsp;<span class="highlight">cultivate</span>&nbsp; new connections, and &nbsp;<span class="highlight">grow</span>&nbsp; vibrant communities online.
             </p>
             </div>
             <div className="watering-can">
