de1ad3c4b0c57a3b704ae55c2ea55f9debec62e9
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2024-03-03 11:59:31 -0500

    layout styling

--- my-portfolio/src/components/nav.js
+++ my-portfolio/src/components/nav.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "../styling/nav.css";
 
-function PortfolioNav () {
+function PortfolioNav() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0 && window.innerWidth <= 900) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []); // Empty dependency array ensures the effect runs only once, similar to componentDidMount
+
     return (
-       <nav className="invert-property">
-         <ul>
-             <li><a href="/">home</a></li>
-             <li><a href="/projects">work</a></li>
-             <li><a href="/contact">contact</a></li>
-          </ul>
-       </nav>
+        <nav className={`invert-property ${isScrolled && 'nav-scrolled'}`}>
+            <ul className={`${isScrolled && 'scrolled'}`}>
+                <li><a href="/">home</a></li>
+                <li><a href="/projects">work</a></li>
+                <li><a href="/contact">contact</a></li>
+            </ul>
+        </nav>
     );
 }
 
