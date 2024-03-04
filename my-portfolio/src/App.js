33878ed70f0fc577c2b8d655563003ae57304328
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2024-03-04 18:16:31 -0500

    new styling and projects and contact page added

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,22 +1,24 @@
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import PageNotFound from './pages/PageNotFound';
-import ShapeArt from './components/shapeArt';
-
+import ShapeArt from './components/shapeArt'; // Assuming you have a ShapeArt component
 
 const App = () => {
+  const location = useLocation();
+  const showShapeArt = !['/projects', '/contact'].includes(location.pathname); // Exclude Projects and Contact pages from showing the ShapeArt
+
   return (
-      <div className="my-portfolio">
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="*" element={<PageNotFound />} />
-          </Routes>
-          <ShapeArt />
-      </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {showShapeArt && <ShapeArt />}
+    </div>
   );
 }
 
--- my-portfolio/src/pages/Projects.js
+++ my-portfolio/src/pages/Projects.js
@@ -2,10 +2,19 @@ import React from 'react';
 import "../index.css";
 
 
+
 export const Projects = () => {
     return (
-        <div> 
-    
+        <div className='work-container'>
+        <div className='work-background'> 
+            <div className='Playdates-title-container'>
+          <h1 className='Playdates-title'>Playdates</h1>
+          <a href="/project-details" className="Playdates-link">visit website</a>
+          <a></a>
+          </div>
+        <h2 className='Playdates-description'>Playdates is a MERN stack application with CRUD operations for dog walking, sitting and boarding on the Upper East Side of New York City. Users can schedule playdates for their dogs using the calendar module, and administrators can manage all bookings and client information internally.</h2>
+        </div>
+       <div className='work-picture-description'> Background by&nbsp;<a href="https://www.freepik.com/free-vector/hand-drawn-groovy-flower-pattern_81074031.htm#fromView=search&page=3&position=24&uuid=fc64ab08-202a-4a04-b9d6-cda1a3397715">Freepik</a></div>
         </div>
     );
 }
