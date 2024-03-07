738e236bb378785cffb0e45c136049bf6cf9384b
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2024-03-07 14:55:33 -0500

    animation

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -2,22 +2,22 @@ import React from 'react';
 import "../index.css";
 import WateringCan from '../components/wateringCan';
 
-export const Home = ({ handleWatering, watered }) => { // Receive handleWatering & watered as prop
- 
-    return (
-      <div className="home-page"> 
-        <p className= "about-me invert-on-black">
-          Hey there! My name is Ella. I'm a full stack software engineer based in New York City. <br /><br /><br />
-          I <span className="highlight">transform</span> brands, <span className="highlight">cultivate</span> new connections, and <span className="highlight">grow</span> vibrant communities online.
-        </p>
-        <div className="watering-can invert-on-black">
-          <WateringCan onWater={handleWatering} /> {/* Pass handleWatering to WateringCan */}
-          <p className="watering-instructions">click me</p>
-        </div>
-        <div className={`black-background ${watered ? 'hidden' : ''}`}></div>
-        <div className="white-background"></div>
+export const Home = ({ handleWatering, watered, singlePageMode }) => {
+  return (
+    <div className="home-page"> 
+      <p className="about-me invert-on-black flash">
+        Hey there! My name is Ella. I'm a full stack software engineer based in New York City. <br /><br /><br />
+        I <span className="highlight">transform</span> brands, <span className="highlight">cultivate</span> new connections, and <span className="highlight">grow</span> vibrant communities online.
+      </p>
+      <div className="watering-can invert-on-black">
+        <WateringCan onWater={handleWatering} /> {/* Pass handleWatering to WateringCan */}
+        <p className="watering-instructions">click me</p>
       </div>
-    );
-  };
+      <div className={`black-background ${watered ? 'hidden' : ''}`}></div>
+      <div className="white-background"></div>
+    </div>
+  );
+};
   
-  export default Home;
+export default Home;
+
